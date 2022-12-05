e364fddf3b59b3e9e98272130ae98a616728584f
sejal-phatangare <115882558+sejal-phatangare@users.noreply.github.com>
2022-12-06 01:14:33 +0530

    Update Room_info.java

--- Room_info.java
+++ Room_info.java
@@ -1,27 +1,124 @@
-package com.company.info;
+package com.company.model;
 
-import com.company.model.CategoryRooms;
-import com.company.model.Room;
+public class Room {
+    private int rId;
+    private String uniqueId;
 
-public class Room_info {
+    //1,2,3,4,5
+    private int rating;
+    //single , double triple , Queen sized, king sized , twin , hollywood twin room , double double , studio , Suite , Presidential suite
+    private int beds;
+    private String usageStatus;
+    private boolean isAC;
 
-    //data
-    public Room data(double price){
+    //deluxe , super deluxe
+    private String speciality;
 
-        Room r=null;
-        //take info
+    private String description;
 
-        return r;
+    private Customer c;
+
+    public Room(int rId, String uniqueId, int rating, int beds, String usageStatus, boolean isAC, String speciality, String description) {
+        this.rId = rId;
+        this.uniqueId = uniqueId;
+        this.rating = rating;
+        this.beds = beds;
+        this.usageStatus = usageStatus;
+        this.isAC = isAC;
+        this.speciality = speciality;
+        this.description = description;
+        c=null;
+    }
+    public Room(){
+
     }
 
-    //add room
-    public void addRoom(CategoryRooms root){
-
+    public int getrId() {
+        return rId;
     }
 
-    //deleteroom
+    public void setrId(int rId) {
+        this.rId = rId;
+    }
 
-    //update room details
+    public String getUniqueId() {
+        return uniqueId;
+    }
+
+    public void setUniqueId(String uniqueId) {
+        this.uniqueId = uniqueId;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
 
 
+    public int getBeds() {
+        return beds;
+    }
+
+    public void setBeds(int beds) {
+        this.beds = beds;
+    }
+
+    public String getUsageStatus() {
+        return usageStatus;
+    }
+
+    public void setUsageStatus(String usageStatus) {
+        this.usageStatus = usageStatus;
+    }
+
+    public boolean isAC() {
+        return isAC;
+    }
+
+    public void setAC(boolean AC) {
+        isAC = AC;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Customer getC() {
+        return c;
+    }
+
+    public void setC(Customer c) {
+        this.c = c;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "rId=" + rId +
+                ", uniqueId='" + uniqueId + '\'' +
+                ", rating=" + rating +
+                ", beds=" + beds +
+                ", usageStatus='" + usageStatus + '\'' +
+                ", isAC=" + isAC +
+                ", speciality='" + speciality + '\'' +
+                ", description='" + description + '\'' +
+                ", c=" + c +
+                '}';
+    }
 }
+
